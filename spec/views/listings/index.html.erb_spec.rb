ce523fb3b9410a466731af4359d99ed250e33f04
JaredGold <jegoldstein94@gmail.com>
2021-02-11 12:13:30 +1100

    create initial listing

---
+++ spec/views/listings/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "listings/index", type: :view do
+  before(:each) do
+    assign(:listings, [
+      Listing.create!(
+        title: "Title",
+        description: "MyText",
+        price: "",
+        sold: false
+      ),
+      Listing.create!(
+        title: "Title",
+        description: "MyText",
+        price: "",
+        sold: false
+      )
+    ])
+  end
+
+  it "renders a list of listings" do
+    render
+    assert_select "tr>td", text: "Title".to_s, count: 2
+    assert_select "tr>td", text: "MyText".to_s, count: 2
+    assert_select "tr>td", text: "".to_s, count: 2
+    assert_select "tr>td", text: false.to_s, count: 2
+  end
+end
